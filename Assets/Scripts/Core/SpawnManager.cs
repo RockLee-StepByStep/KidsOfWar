cf4ce1266292b44ad22d17527542685d216a5655
Michael Dunaev <mih.dunaev@gmail.com>
2022-11-19 18:21:41 +0100

    Refactor SpawnManager class

--- Assets/Scripts/Core/SpawnManager.cs
+++ Assets/Scripts/Core/SpawnManager.cs
@@ -1,57 +1,54 @@
 using System.Collections.Generic;
+using Managers;
 using UnityEngine;
 
-
-namespace HomeWork.Manareg
+namespace Core
 {
     public class SpawnManager : MonoBehaviour
     {
-        [SerializeField] GameObject prefub;
-        //������ ����������
-        [SerializeField] List<GameObject> players = new List<GameObject>(4);
-        //������ ����� �������� 
-        [SerializeField] List<Transform> ListForPoints = new List<Transform>();
+        [SerializeField] private GameObject _prefab;
 
-        int countOfCharacterInOneTeam = 4;
+        private readonly List<GameObject> _players = new List<GameObject>(4);
+        private readonly List<Transform> _listForPoints = new List<Transform>();
+
+        private const int CountOfCharacterInOneTeam = 4;
 
 
-        private PlayerManager Manager;
-        [SerializeField] private int numberOfPlayer = 0;
+        private PlayerManager _manager;
+        private int _numberOfPlayer = 0;
 
-        void Start()
+        private void Start()
         {
             GetPointsListPosition();
             GetThisPlayers();
-            Manager = FindObjectOfType<PlayerManager>();
+            _manager = FindObjectOfType<PlayerManager>();
         }
-        private void Update()
-        {
-            
-        }
+
         private void GetPointsListPosition()
         {
-            var Listic = GetComponentsInChildren<Transform>();
-            ListForPoints.AddRange(Listic);
+            var list = GetComponentsInChildren<Transform>();
+            _listForPoints.AddRange(list);
         }
 
         private void GetThisPlayers()
         {
-            for (int i = 0; i < countOfCharacterInOneTeam; i++)
+            for (int i = 0; i < CountOfCharacterInOneTeam; i++)
             {
-                GameObject both = Instantiate(prefub, ListForPoints[i + 1].position, ListForPoints[i].rotation);
-                players.Add(both);
+                GameObject both = Instantiate(_prefab, _listForPoints[i + 1].position, _listForPoints[i].rotation);
+                _players.Add(both);
             }
         }
 
 
         public GameObject ChooseNextPlayer()
         {
-            Manager.MoveControl(players[numberOfPlayer]);
-            numberOfPlayer += 1;
-            if (numberOfPlayer == players.Count) numberOfPlayer = 0;
-            return players[numberOfPlayer];
+            _manager.MoveControl(_players[_numberOfPlayer]);
+            _numberOfPlayer += 1;
+            if (_numberOfPlayer == _players.Count)
+            {
+                _numberOfPlayer = 0;
+            }
+            return _players[_numberOfPlayer];
         }
-
-        
     }
 }
